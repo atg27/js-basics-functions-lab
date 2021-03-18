d50b4c61101e5a2c6a95594868b07f478de15fd3
Andrew <atg27@uw.edu>
2021-03-18 16:46:14 -0700

    commit tree 69fa636fa2550b3b58d648fcdfe047f29cd51b96

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (pickup) {
+  const blocks = Math.abs(42 - pickup);
+  return blocks;
+ } 
+
+ function distanceFromHqInFeet (pickup) {
+  const feet = (Math.abs(42 - pickup) * 264);
+  return feet;
+ } 
+
+ function distanceTravelledInFeet (start, stop) {
+  const feet = (Math.abs(start - stop) * 264);
+  return feet;
+ } 
+
+ function calculatesFarePrice (start, destination) {
+  const fareDistance = (Math.abs(start - destination) * 264);
+  if (fareDistance <= 400) {
+    return 0;
+  } else if (fareDistance > 400 && fareDistance <= 2000) {
+    const shortRide = ((fareDistance - 400) * 0.02) ;
+    return shortRide;
+  } else if (fareDistance > 2000 && fareDistance < 2500) {
+    
+    return 25;
+  } else if (fareDistance >= 2500) {
+    return 'cannot travel that far';
+  }
+ } 
